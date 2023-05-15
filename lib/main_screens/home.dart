709807a31f707ef024aac3d951f6dd9f7114bb42
robertotonde <robertotonde316@gmail.com>
2023-05-15 10:37:15 +0300

    search bar implementation

--- lib/main_screens/home.dart
+++ lib/main_screens/home.dart
@@ -1,5 +1,7 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:multistore_firebase/minor_screens/search.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -17,38 +19,85 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
-          title: const CupertinoSearchTextField(),
+          title: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SeacrchScreen()));
+            },
+            child: Container(
+              height: 35,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.yellow, width: 1.4),
+                  borderRadius: BorderRadius.circular(25)),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: const [
+                        Padding(
+                          padding: EdgeInsets.symmetric(horizontal: 10),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.grey,
+                          ),
+                        ),
+                        Text(
+                          'What are you looking For',
+                          style: TextStyle(fontSize: 18, color: Colors.grey),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      height: 32,
+                      width: 75,
+                      decoration: BoxDecoration(
+                          color: Colors.yellow,
+                          borderRadius: BorderRadius.circular(25)),
+                      child: const Center(
+                        child: Text(
+                          'Search',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ),
+                    )
+                  ]),
+            ),
+          ),
           bottom: const TabBar(
-            isScrollable: true,
-            tabs: [
-            ReapetedTab(
-              label: "Men",
-            ),
-            ReapetedTab(
-              label: "Women",
-            ),
-            ReapetedTab(
-              label: "Shoes",
-            ),
-            ReapetedTab(
-              label: "Bags",
-            ),
-            ReapetedTab(
-              label: "Elecronics",
-            ),
-            ReapetedTab(
-              label: "Accesories",
-            ),
-            ReapetedTab(
-              label: "Home and Grden",
-            ),
-            ReapetedTab(
-              label: "Kids",
-            ),
-            ReapetedTab(
-              label: "Beuty",
-            ),
-          ]),
+              isScrollable: true,
+              indicatorColor: Colors.yellow,
+              indicatorWeight: 8,
+              tabs: [
+                ReapetedTab(
+                  label: "Men",
+                ),
+                ReapetedTab(
+                  label: "Women",
+                ),
+                ReapetedTab(
+                  label: "Shoes",
+                ),
+                ReapetedTab(
+                  label: "Bags",
+                ),
+                ReapetedTab(
+                  label: "Elecronics",
+                ),
+                ReapetedTab(
+                  label: "Accesories",
+                ),
+                ReapetedTab(
+                  label: "Home and Grden",
+                ),
+                ReapetedTab(
+                  label: "Kids",
+                ),
+                ReapetedTab(
+                  label: "Beauty",
+                ),
+              ]),
         ),
         body: const TabBarView(
           children: [
@@ -60,7 +109,7 @@ class _HomeScreenState extends State<HomeScreen> {
             Center(child: Text("Accesories screen")),
             Center(child: Text("Home and Grden screen")),
             Center(child: Text("Kids screen")),
-            Center(child: Text("Kids screen")),
+            Center(child: Text("Beauty  screen")),
             // Center(child: Text("electonics screen")),
           ],
         ),
