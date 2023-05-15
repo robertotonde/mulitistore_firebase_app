e731205dd1901ee79725af3e5b603dbbb28f7123
robertotonde <robertotonde316@gmail.com>
2023-05-15 09:25:14 +0300

    adding tabview on home screen

--- lib/main_screens/home.dart
+++ lib/main_screens/home.dart
@@ -12,12 +12,73 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
-      length: 3,
+      length: 9,
       child: Scaffold(
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
-          title: const CupertinoSearchTextField(),),
+          title: const CupertinoSearchTextField(),
+          bottom: const TabBar(
+            isScrollable: true,
+            tabs: [
+            ReapetedTab(
+              label: "Men",
+            ),
+            ReapetedTab(
+              label: "Women",
+            ),
+            ReapetedTab(
+              label: "Shoes",
+            ),
+            ReapetedTab(
+              label: "Bags",
+            ),
+            ReapetedTab(
+              label: "Elecronics",
+            ),
+            ReapetedTab(
+              label: "Accesories",
+            ),
+            ReapetedTab(
+              label: "Home and Grden",
+            ),
+            ReapetedTab(
+              label: "Kids",
+            ),
+            ReapetedTab(
+              label: "Beuty",
+            ),
+          ]),
+        ),
+        body: const TabBarView(
+          children: [
+            Center(child: Text("men screen")),
+            Center(child: Text("women screen")),
+            Center(child: Text("shoes screen")),
+            Center(child: Text("Bags screen")),
+            Center(child: Text("Elecronics screen")),
+            Center(child: Text("Accesories screen")),
+            Center(child: Text("Home and Grden screen")),
+            Center(child: Text("Kids screen")),
+            Center(child: Text("Kids screen")),
+            // Center(child: Text("electonics screen")),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ReapetedTab extends StatelessWidget {
+  final String label;
+  const ReapetedTab({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return Tab(
+      child: Text(
+        label,
+        style: TextStyle(color: Colors.grey.shade600),
       ),
     );
   }
