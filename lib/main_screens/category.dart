3d7a15486545d2f8376d030bc35b043186a81bfe
robertotonde <robertotonde316@gmail.com>
2023-05-17 10:53:28 +0300

    category changes view and side navigator

--- lib/main_screens/category.dart
+++ lib/main_screens/category.dart
@@ -21,6 +21,20 @@ class CategoryScreen extends StatefulWidget {
 }
 
 class _CategoryScreenState extends State<CategoryScreen> {
+  final PageController _pageController = PageController();
+
+  @override
+  void initState() {
+    for (var element in items) {
+      element.isSelected = false;
+    }
+    setState(() {
+      items[0].isSelected = true;
+    });
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
@@ -49,12 +63,15 @@ class _CategoryScreenState extends State<CategoryScreen> {
           itemBuilder: (context, index) {
             return GestureDetector(
               onTap: () {
-                for (var element in items) {
-                  element.isSelected = false;
-                }
-                setState(() {
-                  items[index].isSelected = true;
-                });
+                _pageController.animateToPage(index,
+                    duration: const Duration(milliseconds: 100),
+                    curve: Curves.bounceInOut);
+                // for (var element in items) {
+                //   element.isSelected = false;
+                // }
+                // setState(() {
+                //   items[index].isSelected = true;
+                // });
               },
               child: Container(
                   color: items[index].isSelected == true
@@ -72,6 +89,47 @@ class _CategoryScreenState extends State<CategoryScreen> {
       height: size.height * 0.8,
       width: size.width * 0.8,
       color: Colors.white,
+      child: PageView(
+        controller: _pageController,
+        onPageChanged: (value) {
+          for (var element in items) {
+            element.isSelected = false;
+          }
+          setState(() {
+            items[value].isSelected = true;
+          });
+        },
+        scrollDirection: Axis.vertical,
+        children: const [
+          Center(
+            child: Text('men category '),
+          ),
+          Center(
+            child: Text('women category '),
+          ),
+          Center(
+            child: Text('shoes category '),
+          ),
+          Center(
+            child: Text('bags category '),
+          ),
+          Center(
+            child: Text(' electronics category '),
+          ),
+          Center(
+            child: Text('accesories category '),
+          ),
+          Center(
+            child: Text('home & garden category '),
+          ),
+          Center(
+            child: Text('kids category '),
+          ),
+          Center(
+            child: Text('beauty category '),
+          ),
+        ],
+      ),
     );
   }
 }
