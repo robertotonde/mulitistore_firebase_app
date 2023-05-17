0921ff856cf25b1b26c55f9ea15459149033d38a
robertotonde <robertotonde316@gmail.com>
2023-05-17 14:03:23 +0300

    subcategory

---
+++ lib/categories/men_categ.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:multistore_firebase/core/utilities/categ_list.dart';
+import '../presentation/screens/minor_screens/subcategory_products.dart';
+
+
+// List<String> imagetry = [
+//   'images/try/image0.jpg'
+//   'images/try/image1.jpg'
+//   'images/try/image2.jpg'
+//   'images/try/image3.jpg'
+// ];
+// List<String> labeletry = [
+//   'shirt',
+//   'jeans',
+//   'shoes',
+//   'jacket',
+// ];
+
+class MenCategory extends StatelessWidget {
+  const MenCategory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.all(30.0),
+          child: Text(
+            'men',
+            style: TextStyle(
+                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5),
+          ),
+        ),
+        SizedBox(
+          height: MediaQuery.of(context).size.height * 0.68,
+          child: GridView.count(
+              mainAxisSpacing: 70,
+              crossAxisSpacing: 15,
+              crossAxisCount: 3,
+              children: List.generate(men.length, (index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => SubCategoryProducts(
+                                mainCategName: 'Men',
+                                  subCategName: men[index],
+                                )));
+                  },
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: 70,
+                        width: 70,
+                        child: Image(
+                          image: AssetImage('images/men/men$index.jpg'),
+                        ),
+                      ),
+                      Text(men[index])
+                    ],
+                  ),
+                );
+              })),
+        )
+      ],
+    );
+  }
+}
