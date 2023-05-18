a4ca38b2d5f637ab253113dd66b099dd10b7d1ee
robertotonde <robertotonde316@gmail.com>
2023-05-18 13:19:04 +0300

    adding slide bar on category

--- lib/presentation/screens/categories/men_categ.dart
+++ lib/presentation/screens/categories/men_categ.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
-
-import '../../../core/utilities/categ_list.dart';
-import '../minor_screens/subcategory_products.dart';
+import 'package:multistore_firebase/core/utilities/categ_list.dart';
+import 'package:multistore_firebase/presentation/screens/minor_screens/subcategory_products.dart';
 
 // List<String> imagetry = [
 //   'images/try/image0.jpg'
@@ -21,50 +20,115 @@ class MenCategory extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const Padding(
-          padding: EdgeInsets.all(30.0),
-          child: Text(
-            'men',
-            style: TextStyle(
-                fontSize: 24, fontWeight: FontWeight.bold, letterSpacing: 1.5),
-          ),
-        ),
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.68,
-          child: GridView.count(
-              mainAxisSpacing: 70,
-              crossAxisSpacing: 15,
-              crossAxisCount: 3,
-              children: List.generate(men.length, (index) {
-                return GestureDetector(
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => SubCategoryProducts(
-                                mainCategName: 'Men',
-                                  subCategName: men[index],
-                                )));
-                  },
-                  child: Column(
-                    children: [
-                      SizedBox(
-                        height: 70,
-                        width: 70,
-                        child: Image(
-                          image: AssetImage('images/men/men$index.jpg'),
-                        ),
-                      ),
-                      Text(men[index])
-                    ],
+    return Padding(
+      padding: const EdgeInsets.all(5.0),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.8,
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.all(30.0),
+                    child: Text(
+                      'men',
+                      style: TextStyle(
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                          letterSpacing: 1.5),
+                    ),
                   ),
-                );
-              })),
-        )
-      ],
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.68,
+                    child: GridView.count(
+                        mainAxisSpacing: 70,
+                        crossAxisSpacing: 15,
+                        crossAxisCount: 3,
+                        children: List.generate(men.length, (index) {
+                          return GestureDetector(
+                            onTap: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => SubCategoryProducts(
+                                            mainCategName: 'Men',
+                                            subCategName: men[index],
+                                          )));
+                            },
+                            child: Column(
+                              children: [
+                                SizedBox(
+                                  height: 70,
+                                  width: 70,
+                                  child: Image(
+                                    image: AssetImage('images/men/men$index.jpg'),
+                                  ),
+                                ),
+                                Text(men[index])
+                              ],
+                            ),
+                          );
+                        })),
+                  )
+                ],
+              ),
+            ),
+          ),
+          //vertical sizized box should display
+          Positioned(
+              bottom: 0,
+              right: 0,
+              child: SizedBox(
+                height: MediaQuery.of(context).size.height * 0.8,
+                width: MediaQuery.of(context).size.width * 0.05,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 40),
+                  child: Container(
+                    decoration: BoxDecoration(
+                        color: Colors.brown.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(50)),
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children:  [
+                         const Text(
+                            ' << ',
+                            style: TextStyle(
+                                color: Colors.brown,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 10),
+                          ),
+                          Text(
+                            'men'.toUpperCase(),
+                            style: const TextStyle(
+                                color: Colors.brown,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 10),
+                          ),
+                      const     Text(
+                            ' >>',
+                            style: TextStyle(
+                                color: Colors.brown,
+                                fontSize: 16,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 10),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ))
+        ],
+      ),
     );
   }
 }
