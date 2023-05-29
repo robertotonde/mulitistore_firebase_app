580ca0944003e75432c87aeffc94eadc6c22fa78
robertotonde <robertotonde316@gmail.com>
2023-05-29 13:12:04 +0300

    creating profile screen"

--- lib/presentation/screens/main_screens/customer_home.dart
+++ lib/presentation/screens/main_screens/customer_home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:multistore_firebase/presentation/screens/main_screens/cart.dart';
+import 'package:multistore_firebase/presentation/screens/main_screens/profile.dart';
 import 'package:multistore_firebase/presentation/screens/main_screens/stores.dart';
 
 import 'category.dart';
@@ -19,7 +20,7 @@ class _CustomerHomeScreenState extends State<CustomerHomeScreen> {
     CategoryScreen(),
     StoresScreen(),
     CartScreen(),
-    Center(child: Text('Profile screen')),
+    ProfileScreen()
   ];
   @override
   Widget build(BuildContext context) {
---
+++ lib/presentation/screens/main_screens/profile.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class ProfileScreen extends StatefulWidget {
+  const ProfileScreen({super.key});
+
+  @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            elevation: 0,
+            backgroundColor: Colors.white,
+            expandedHeight: 140,
+            flexibleSpace: LayoutBuilder(
+              builder: (context, constraints) {
+                return FlexibleSpaceBar(
+                  title: AnimatedOpacity(
+                    duration: const Duration(milliseconds: 200),
+                    opacity: constraints.biggest.height <= 120 ? 1 : 0,
+                    child: const Text('Account'),
+                  ),
+                  background: Container(
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                            colors: [Colors.yellow, Colors.brown])),
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
