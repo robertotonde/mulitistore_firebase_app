ee6089833e3f42b22f68da10f3fa700d76b5eed4
jayvibespro <jacksonsteven436@gmail.com>
2023-06-02 16:04:47 +0300

    some change

--- lib/presentation/screens/main_screens/profile.dart
+++ lib/presentation/screens/main_screens/profile.dart
@@ -12,9 +12,12 @@ class _ProfileScreenState extends State<ProfileScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
+        physics: const BouncingScrollPhysics(),
         slivers: [
           SliverAppBar(
             elevation: 0,
+            stretch: true,
+            pinned: true,
             backgroundColor: Colors.white,
             expandedHeight: 140,
             flexibleSpace: LayoutBuilder(
@@ -23,7 +26,10 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   title: AnimatedOpacity(
                     duration: const Duration(milliseconds: 200),
                     opacity: constraints.biggest.height <= 120 ? 1 : 0,
-                    child: const Text('Account'),
+                    child: const Text(
+                      'Account',
+                      style: TextStyle(color: Colors.black54),
+                    ),
                   ),
                   background: Container(
                     decoration: const BoxDecoration(
@@ -32,6 +38,25 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   ),
                 );
               },
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.only(top: 20.0, left: 20, right: 20),
+                  child: Center(
+                      child: Container(
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.red.withOpacity(0.1),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                  )),
+                );
+              },
+              childCount: 20,
             ),
           )
         ],
