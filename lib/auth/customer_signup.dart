e3dc9bb572bb5cf0a258cdcb1392dfc71a108de9
robertotonde <robertotonde316@gmail.com>
2023-06-19 09:17:32 +0300

    saving auth data

--- lib/auth/customer_signup.dart
+++ lib/auth/customer_signup.dart
@@ -3,6 +3,10 @@
 import 'package:flutter/material.dart';
 import 'package:multistore_firebase/presentation/components/auth_widget.dart';
 
+// final TextEditingController _nameController = TextEditingController();
+// final TextEditingController _emailController = TextEditingController();
+// final TextEditingController _passwordController = TextEditingController();
+
 class CustomerRegister extends StatefulWidget {
   const CustomerRegister({super.key});
 
@@ -11,6 +15,11 @@ class CustomerRegister extends StatefulWidget {
 }
 
 class _CustomerRegisterState extends State<CustomerRegister> {
+  late String name;
+  late String email;
+  late String password;
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool passwordVisible = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,97 +30,161 @@ class _CustomerRegisterState extends State<CustomerRegister> {
             reverse: true,
             child: Padding(
               padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: [
-                  const AuthHeaderLabel(
-                    headerLabel: 'Sign Up',
-                  ),
-                  Row(
-                    children: [
-                      const Padding(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 20, horizontal: 40),
-                        child: CircleAvatar(
-                          radius: 60,
-                          backgroundColor: Colors.purpleAccent,
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    const AuthHeaderLabel(
+                      headerLabel: 'Sign Up',
+                    ),
+                    Row(
+                      children: [
+                        const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 20, horizontal: 40),
+                          child: CircleAvatar(
+                            radius: 60,
+                            backgroundColor: Colors.purpleAccent,
+                          ),
                         ),
+                        Column(
+                          children: [
+                            Container(
+                              decoration: const BoxDecoration(
+                                  color: Colors.purple,
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(15),
+                                      topRight: Radius.circular(15))),
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.camera_alt,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  print('pick image from camera');
+                                },
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 6,
+                            ),
+                            Container(
+                              decoration: const BoxDecoration(
+                                  color: Colors.purple,
+                                  borderRadius: BorderRadius.only(
+                                      bottomLeft: Radius.circular(15),
+                                      bottomRight: Radius.circular(15))),
+                              child: IconButton(
+                                icon: const Icon(
+                                  Icons.photo,
+                                  color: Colors.white,
+                                ),
+                                onPressed: () {
+                                  print('pick image from gallery');
+                                },
+                              ),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'please enter your full name ';
+                          }
+                          return null;
+                        },
+                        onChanged: (value) {
+                          name = value;
+                        },
+                        //   controller: _nameController,
+                        decoration: texFormDecoration.copyWith(
+                            labelText: 'Full Name',
+                            hintText: 'Enter your full name '),
                       ),
-                      Column(
-                        children: [
-                          Container(
-                            decoration: const BoxDecoration(
-                                color: Colors.purple,
-                                borderRadius: BorderRadius.only(
-                                    topLeft: Radius.circular(15),
-                                    topRight: Radius.circular(15))),
-                            child: IconButton(
-                              icon: const Icon(
-                                Icons.camera_alt,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                print('pick image from camera');
-                              },
-                            ),
-                          ),
-                          const SizedBox(
-                            height: 6,
-                          ),
-                          Container(
-                            decoration: const BoxDecoration(
-                                color: Colors.purple,
-                                borderRadius: BorderRadius.only(
-                                    bottomLeft: Radius.circular(15),
-                                    bottomRight: Radius.circular(15))),
-                            child: IconButton(
-                              icon: const Icon(
-                                Icons.photo,
-                                color: Colors.white,
-                              ),
-                              onPressed: () {
-                                print('pick image from gallery');
-                              },
-                            ),
-                          )
-                        ],
-                      )
-                    ],
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: TextFormField(
-                      decoration: texFormDecoration.copyWith(
-                          labelText: 'Full Name',
-                          hintText: 'Enter your full name '),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: TextFormField(
-                      keyboardType: TextInputType.emailAddress,
-                      decoration: texFormDecoration.copyWith(
-                          labelText: 'Email Address',
-                          hintText: 'Enter your email address '),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'please enter your email Adress';
+                          } else if (value.isValidEmail() == false) {
+                            return 'invalid email';
+                          } else if (value.isValidEmail() == true) {
+                            return null;
+                          }
+                          return null;
+                        },
+                          onChanged: (value) {
+                          email = value;
+                        },
+                        //  controller: _emailController,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: texFormDecoration.copyWith(
+                            labelText: 'Email Address',
+                            hintText: 'Enter your email address '),
+                      ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 10),
-                    child: TextFormField(
-                      decoration: texFormDecoration.copyWith(
-                          labelText: 'Password',
-                          hintText: 'Enter your pasword'),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(vertical: 10),
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value!.isEmpty) {
+                            return 'please enter your password ';
+                          }
+                          return null;
+                        },
+                          onChanged: (value) {
+                          password = value;
+                        },
+                        //  controller: _passwordController,
+                        obscureText: passwordVisible,
+                        decoration: texFormDecoration.copyWith(
+                            suffixIcon: IconButton(
+                                onPressed: () {
+                                  setState(() {
+                                    passwordVisible = !passwordVisible;
+                                  });
+                                },
+                                icon: Icon(
+                                  passwordVisible
+                                      ? Icons.visibility
+                                      : Icons.visibility_off,
+                                  color: Colors.purple,
+                                )),
+                            labelText: 'Password',
+                            hintText: 'Enter your pasword'),
+                      ),
                     ),
-                  ),
-                  HaveAccount(
-                    haveAccount: 'already have account',
-                    actionLabel: 'Log In',
-                    onPressed: () {},
-                  ),
-                  AuthMainButton(
-                    mainButtonLabel: 'Sign Up',
-                    onPressed: () {},
-                  )
-                ],
+                    HaveAccount(
+                      haveAccount: 'already have account',
+                      actionLabel: 'Log In',
+                      onPressed: () {},
+                    ),
+                    AuthMainButton(
+                      mainButtonLabel: 'Sign Up',
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          print('valid');
+                          // setState(() {
+                          //   name = _nameController.text;
+                          //   email = _emailController.text;
+                          //   password = _passwordController.text;
+                          // });
+                          print(name);
+                          print(email);
+                          print(password);
+                        } else {
+                          print('not valid');
+                        }
+                      },
+                    )
+                  ],
+                ),
               ),
             ),
           ),
@@ -120,5 +193,3 @@ class _CustomerRegisterState extends State<CustomerRegister> {
     );
   }
 }
-
-
